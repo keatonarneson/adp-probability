40fb0e3d8b87f6baaec26c3f6f31f274ede97575
Keaton <keaton.arneson@gmail.com>
2021-02-24 17:10:23 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Papa from 'papaparse';
+import NormalDistribution from 'normal-distribution';
+import csv from './static/adp-probability.tsv';
 
 function App() {
+  const [rows, setRows] = useState(null);
+  const [draftSlot, setDraftSlot] = useState(1);
+
+  const probability = (mean, standardDeviation, draftSlot) => {
+    const normDist = new NormalDistribution(mean, standardDeviation);
+
+    return 1 - normDist.cdf(draftSlot);
+  };
+
+  const handleChange = e => {
+    setDraftSlot(e.target.value);
+  };
+
+  // Prevent Enter Key from reloading the page
+  const onKeyDown = e => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+    }
+  };
+
+  useEffect(() => {
+    Papa.parse(csv, {
+      download: true,
+      header: true,
+      complete: data => {
+        setRows(data);
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>ADP Probability of Availability</h1>
+        <p>NFBC 12-team Online Championship ADP</p>
+        <small>
+          <em>All drafts up to 2-24-2021</em>
+        </small>
       </header>
+      <form>
+        <label>Draft Slot</label>
+        <input
+          type="number"
+          min="1"
+          value={draftSlot}
+          onChange={handleChange}
+          onKeyDown={onKeyDown}
+        />
+      </form>
+      <div className="table-wrapper">
+        <table>
+          <thead>
+            <tr>
+              {rows?.meta.fields.map((column, index) => (
+                <th key={index}>{column}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {rows?.data.map((row, index) => (
+              <tr key={index}>
+                <td>{row.Rank}</td>
+                <td>{row.Player}</td>
+                <td>{row.Team}</td>
+                <td>{row.Position}</td>
+                <td>{row.ADP}</td>
+                <td>{row.Min_Pick}</td>
+                <td>{row.Max_Pick}</td>
+                <td>{row.Difference}</td>
+                <td>{row.StDev}</td>
+                <td>{draftSlot}</td>
+                <td>
+                  <strong>
+                    {(probability(row.ADP, row.StDev, draftSlot) * 100).toFixed(
+                      2
+                    ) + '%'}
+                  </strong>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
